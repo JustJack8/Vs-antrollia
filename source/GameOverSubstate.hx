1b1aae7399d3cbdfc9429b7115db41b3e4aa98f4
Jack <81973755+JustJack8@users.noreply.github.com>
2021-10-08 22:16:27 -0700

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -86,7 +86,9 @@ class GameOverSubstate extends MusicBeatSubstate
 			//pretty sloppy way of adding the death quotes, unless shadow wants to make it better
 			FlxG.sound.playMusic(Paths.music('gameOver' + stageSuffix));
 			startVibin = true;
-			FlxG.sound.play(Paths.soundRandom('DeathQuote', 1, 28), FlxG.random.float(0.1, 0.2));
+			
+			if (PlayState.SONG.player2 == 'antrollia')
+				FlxG.sound.play(Paths.soundRandom('DeathQuote', 1, 28), 1, 27), 1.2);
 		}
 
 		if (FlxG.sound.music.playing)
